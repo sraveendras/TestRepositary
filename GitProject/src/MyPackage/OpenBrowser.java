204846a928a66aec4b355060718dbbdbaa5809ea
RC <RC@Ravi>
2019-01-03 14:37:35 +0530

    Open Browser

---
+++ GitProject/src/MyPackage/OpenBrowser.java
@@ -0,0 +1,20 @@
+package MyPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenBrowser {
+
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://google.com");
+		System.out.println(driver.getCurrentUrl());
+			
+		
+	}
+
+}
